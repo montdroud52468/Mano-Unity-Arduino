bccb322f979be46fde1f6bd7e0eafa3c0040c843
Ricardo Daniel Montiel Jacinto <39661323+montdroud52468@users.noreply.github.com>
2022-05-02 19:21:16 -0500

    Add files via upload

---
+++ Mano Unity 3D/Assets/Altura.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class Altura : MonoBehaviour
+{
+    SerialPort serialPort=new SerialPort("COM15",9600);
+
+    void Start(){
+        serialPort.Open();
+        serialPort.ReadTimeout=100;
+
+    }
+    
+    void Update(){
+        try{
+           if(serialPort.IsOpen){
+               print(serialPort.ReadLine());
+           }  
+        }catch (System.Exception ex){     
+            ex=new System.Exception();
+        }
+    }
+    
+}
